cb1e24e3ae6a71c8a431c50459dba20a0ca0b00d
Aleksandar Velichkov <sashko98@gmail.com>
2021-07-09 14:20:18 +0300

    -article view works now

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
-const Article = () => {
-    return (
-        <div></div>
-    )
+import { withRouter } from 'react-router-dom';
+
+async function getArticleData(id) {
+    const res = await fetch(`http://localhost:2345/articles/${id}`);
+    const article = await res.json();
+
+    return article;
 }
 
-export default Article;
+class Article extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            article: {},
+        }
+    }
+
+    async componentDidMount() {
+        const article = await getArticleData(this.props.match.params.id)
+        this.setState({...this.state, article})
+    }
+
+    render() {
+        return (
+            <>
+                <h1>{this.state.article.title}</h1>
+                <p>{this.state.article.body}</p>
+            </>
+        )
+    }
+}
+
+export default withRouter(Article);
