d2486177020d6f6b51b46959b98d998f1f5c2479
Aleksandar Velichkov <sashko98@gmail.com>
2021-07-09 14:58:49 +0300

    -converted Home to function

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,36 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import ArticleList from '../article/List';
 
-const getArticleList = async (perPage, offset) => {
-    try {
-        const res = await fetch('http://localhost:2345/articles/')
-        const articles = await res.json();
+// class Home extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             ArticleList: [],
+//         }
+//     }
 
-        return <ArticleList articles={articles}/>;
-    } catch(e) {
-        console.error(e);
-    }
-}
+//     async componentDidMount() {
+//         const ArticleList = await getArticleList();
+//         this.setState({...this.state, ArticleList})
+//     }
 
-class Home extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            ArticleList: [],
+//     render() {
+//         return(
+//             <ul>{this.state.ArticleList}</ul>
+//         )
+//     }
+// }
+
+function Home() {
+    const [articleList, setArticleList] = useState([]);
+
+    useEffect(() => {
+        async function fetchArticleList(perPage, offset){
+            try {
+                const res = await fetch('http://localhost:2345/articles/')
+                const articles = await res.json();
+        
+                setArticleList(<ArticleList articles={articles}/>);
+            } catch(e) {
+                console.error(e);
+            }
         }
-    }
-
-    async componentDidMount() {
-        const ArticleList = await getArticleList();
-        this.setState({...this.state, ArticleList})
-    }
-
-    render() {
-        return(
-            <ul>{this.state.ArticleList}</ul>
-        )
-    }
+        fetchArticleList();
+    }, [articleList]);
+    return(
+        <ul>{articleList}</ul>
+    )
 }
 
 export default Home;
