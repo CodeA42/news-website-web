e0f4b17a8a0ac970b3c62c6071a1ee54aff91ee2
Aleksandar Velichkov <sashko98@gmail.com>
2022-03-23 11:32:14 +0200

    -added logout link

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -18,6 +18,9 @@ const Navbar = () => {
                 <li>
                     <Link to="/login">Login</Link>
                 </li>
+                <li>
+                    <Link to="/logout">Logout</Link>
+                </li>
             </ul>
         </nav>
     )
