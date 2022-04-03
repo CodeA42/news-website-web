2d82addbadc522f5f9de5ee95859150c8d949f66
Aleksandar Velichkov <sashko98@gmail.com>
2022-04-03 14:47:36 +0300

    Added global context for users

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
@@ -13,36 +13,58 @@ import Login from './components/user/Login';
 import Logout from './components/user/Logout';
 import Register from './components/user/Register';
 import Profile from './components/user/Profile';
+import { UserContext } from './components/context/user';
+import jwt from "jsonwebtoken";
 
 function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    if(localStorage.getItem('accessToken')){
+      const payload = jwt.decode(localStorage.getItem('accessToken'))
+      if(payload.exp > Date.now() / 1000){
+        setUser({
+          id: payload.id,
+          username: payload.username
+        })
+      } else {
+        //TODO: call auth/refresh
+      }
+    } else {
+      //TODO: call auth/refresh
+    }
+  },[])
+
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path={["/", "/home"]}>
-            <Home />
-          </Route>
-          <Route exact path="/article/create">
-            <CreateArticle />
-          </Route>
-          <Route path="/article/:id">
-            <Article/>
-          </Route>
-          <Route path="/login">
-            <Login/>
-          </Route>
-          <Route path="/logout">
-            <Logout/>
-          </Route>
-          <Route path="/register">
-            <Register/>
-          </Route>
-          <Route path='/:id'>
-            <Profile/>
-          </Route>
-        </Switch>
-      </Router>
+      <UserContext.Provider value={{user, setUser}}>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path={["/", "/home"]}>
+              <Home />
+            </Route>
+            <Route exact path="/article/create">
+              <CreateArticle />
+            </Route>
+            <Route path="/article/:id">
+              <Article/>
+            </Route>
+            <Route path="/login">
+              <Login/>
+            </Route>
+            <Route path="/logout">
+              <Logout/>
+            </Route>
+            <Route path="/register">
+              <Register/>
+            </Route>
+            <Route path='/:id'>
+              <Profile/>
+            </Route>
+          </Switch>
+        </Router>
+      </UserContext.Provider>
     </div>
   );
 }
